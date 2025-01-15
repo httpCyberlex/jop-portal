dcc88ea8610a34752f518ee690b7ae82ad77d026
httpCyberlex <ansshubhardwaj@gmail.com>
2025-01-15 21:48:41 +0530

    Add API

--- Candidate-API/CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
+++ Candidate-API/CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16883f5d03786c1f128c2079a6f344e5d6a9dbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
